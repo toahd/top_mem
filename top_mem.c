a5533121bd6de260f99bb296266a969aade04d1c
toahd <toahd.git@outlook.com>
2026-06-14 12:54:11 -0400

    statm does not contain a wholistic set of data to properly capture memory usage

--- top_mem.c
+++ top_mem.c
@@ -9,8 +9,8 @@
 #include <unistd.h>
 
 /*
- *  __________
- * /__   ____/      ___       ___
+ *  _________
+ * /__   ___/       ___       ___
  *    | | ___   ____| |     __| |
  *    | |/ _ \ / _  | |___ / _  |
  *    | | (_) | (_| |  _  | (_| |
@@ -25,67 +25,23 @@
 * [LICENSE] GNU GPLv3
 ******************************************************************************/
 
-// TODOs:
-// - Read from a file other than /proc/$PID/status to avoid extra kernel space
-// overhead. the status file has extraneous information and is around 1500 Kb, 
-// an alternative source to read from would be either statm or stat.
-
 // number has been derived emperically as status files have not been seen to 
-// grow larger than 2KB in size:
-// [toahd@framework13 scripts]$ cat /proc/$$/status | wc -c
-// [toahd@framework13 scripts]$ 1523
-#define BUFFER_SIZE 2048
+// grow larger than 1KB in size:
+// [toahd@framework13 top_mem]$ cat /proc/$$/status | wc -c
+// [toahd@framework13 top_mem]$ 357
+#define BUFFER_SIZE 1024
 
-// the full paths look like /proc/$PID/status
+// the full paths look like /proc/$PID/stat
 // individual $PIDs comes from the dirent stream while looping through /proc
 const char PROC[] = "/proc";
-const char STATUS[] = "/status";
+const char STATUS[] = "/stat";
 
-// key type decides parsing logic when a key is found
-typedef enum { KEY_LONG, KEY_STRING, NONE } key_type;
-
-// holds the values parse from the status_keys
+// holds the values parsed from the stat file
 typedef struct {
 	long vmrss;
 	int pid;
 	char name[40];
 } proc_status;
-
-// key struct to hold key string literal, length of key and enum hint for how
-// to parse the key value
-typedef struct {
-	const char *key;
-	const size_t key_len;
-	key_type type;
-	size_t offset;     // offset into target proc_status field
-	size_t field_size; // size of field, such as name[size]
-} status_key;
-#define KEY_L(k, field) { (k), sizeof(k) - 1, KEY_LONG, \
-	offsetof(proc_status, field), \
-	sizeof(((proc_status*)0)->field) }
-#define KEY_S(k, field) { (k), sizeof(k) - 1, KEY_STRING, \
-	offsetof(proc_status, field), \
-	sizeof(((proc_status*)0)->field) }
-// add back to key_type enum if an INT parser is needed again
-//#define KEY_I(k, field) { (k), sizeof(k) - 1, KEY_INT, \
-//	offsetof(proc_status, field), \
-//	sizeof(((proc_status*)0)->field) }
-
-// full list of keys to search for in the PIDs status file. They are listed 
-// below in the order that they appear in the status files. The status files
-// are walked ONCE from top to bottom and loops through the keys as they are
-// found. keys found that are not the current key will get skipped as a 
-// consequence - so add new keys in order!
-// $ cat /proc/$$/status | grep -En "Name|VmRSS|NSpid"
-// 1:Name:   bash
-// 14:NSpid: 321312
-// 23:VmRSS: 6720 kB
-static status_key status_keys[] = {
-	KEY_S("Name:", name),
-	//KEY_I("NSpid:", pid), optimization: just take from file name
-	KEY_L("VmRSS:", vmrss),
-	{NULL, 0, NONE, 0, 0}
-};
 
 // holds the top 5 memory using procs
 static proc_status top[5] = {0};
@@ -103,58 +59,40 @@ int parse_status(int fd, proc_status *out) {
 	}
 	buff[bytes_read] = '\0';
 	
-	// start looping through status_keys
-	const char *pos = buff;
-	const char *end = buff + bytes_read;
-	const status_key *curr_key = status_keys;
+	// first parse the comm name which is in parenthesis
+	// the comm name can also CONTAIN parenthesis itself!
+	// this is also the filename of the executable when launched
+	const char *first_open_paren = strchr(buff, '(' );
+	const char *last_closed_paren = strrchr(buff, ')' );
 	
-	while(*pos && curr_key->key != NULL) {
-		if (strncmp(pos, curr_key->key, curr_key->key_len) == 0) {	
-			pos += curr_key->key_len;	
-			char *dest = (char *)out + curr_key->offset;
-			
-			if (curr_key->type == KEY_STRING) {
-				while (*pos == ' ' || *pos == '\t')
-					pos++;
-		
-				size_t i = 0;
-				while(i < curr_key->field_size - 1 && *pos && *pos != '\n') {
-					dest[i++] = *pos++;
-				}
-				dest[i] = '\0';
-			} else {
-				// KEY_LONG/KEY_INT logic	
-				while (*pos < '0' || *pos > '9' ) {
-					pos++;
-				}
-			
-				long val = 0;
-				while (*pos >= '0' && *pos <= '9') {
-					val = val * 10 + (*pos++ - '0');
-				}
-				
-				// write the correct number of bytes
-				if (curr_key->type == KEY_LONG) {
-					*(long *)dest = val;
-				} else {
-					*(int *)dest = val;
-				}
-			}
+	// make sure parens exist and indexes line up
+	if(!first_open_paren || !last_closed_paren || last_closed_paren < first_open_paren)
+		return -1;
+	
+	// copy everything inbetween the parenthesis to out->name
+	// same room for null terminator
+	size_t name_len = (size_t)(last_closed_paren - (first_open_paren + 1));
+	if (name_len >= sizeof(out->name))
+		name_len = sizeof(out->name) - 1;
 
-			curr_key++;
-		}
-		
-		// replace this logic with memchr
-		//while(*pos && *pos != '\n') pos++;
-		//if (*pos == '\n') pos++;
-		
-		// Takes advantage of SIMD, instead of reading a single byte at
-		// a time this reads in 32 bytes at a time
-		const char *nl = memchar(pos, '\n', end-pos);
-		if (!nl) break;
-		pos = nl++;
+	memcpy(out->name, first_open_paren + 1, name_len);
+	out->name[name_len] = '\0';
+
+	// move onto parsing vmrss
+	// rss is field 24, which means it follows the 22nd space after the last ')'
+	int spaces = 0;
+	const char *pos = last_closed_paren++;
+	while (*pos && spaces < 22) {
+    		if (*pos == ' ') spaces++;
+    		pos++;
 	}
 
+	long rss = 0;
+	while (*pos >= '0' && *pos <= '9') {
+		rss = rss * 10 + (*pos++ - '0');
+	}
+
+	out->vmrss = rss;
 	return 1;
 }
 
@@ -215,7 +153,8 @@ int main() {
 
 	closedir(dir_stream);
 
+	long page_size = sysconf(_SC_PAGESIZE) / 1024;   // 4096 / 1024 = 4
 	for (size_t i = 0; i < NUM_TOPS; i++)
-		printf("%7d %-16s %ld kB\n", top[i].pid, top[i].name, top[i].vmrss);
+    		printf("%7d %-16s %ld kB\n", top[i].pid, top[i].name, top[i].vmrss * page_size);
 }
 
